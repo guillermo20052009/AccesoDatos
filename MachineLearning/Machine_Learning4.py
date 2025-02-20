c10945764c142b7e45b9ea62863300641bbbade2
Tu Nombre <you@example.com>
2025-02-20 10:14:52 +0100

    20 febrero

---
+++ MachineLearning/Machine_Learning4.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#El dataset seguiremos usando el mismo ya que considero que tiene la información necesaria para realizar las operaciones
+#predicciones que se requieren en este caso.
+#Lectura del CSV
+df = pd.read_csv("all_seasons.csv")
+
+
+
+# La variable objetivo es "pts" (puntos), por lo que se debe separar del resto de variables.
+correlation = df.corr(numeric_only=True)["pts"]
+print(correlation.sort_values(ascending=False))
