e6535174f697d4eb44fc5b25757b7ae6d2e2eda0
Tu Nombre <you@example.com>
2025-02-24 14:40:32 +0100

    24 febrero

--- MachineLearning/Machine_Learning4.py
+++ MachineLearning/Machine_Learning4.py
@@ -10,3 +10,10 @@ df = pd.read_csv("all_seasons.csv")
 # La variable objetivo es "pts" (puntos), por lo que se debe separar del resto de variables.
 correlation = df.corr(numeric_only=True)["pts"]
 print(correlation.sort_values(ascending=False))
+#Vamos a elegir como predictoras udg_pct,ast,reb.
+
+#Identificar valores faltantes
+print(df.isnull().sum())
+
+#Identificar los valores erróneos
+
